5d4bc55e3ec8ff8927aed618b539c50c0ef5af64
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-06-22 01:33:08 +0100

    Added: Contact Section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,15 @@
 const Contact: React.FC = () => {
   return (
     <section>
-      <h1>Contact.</h1>
+      <h1>Get in touch.</h1>
+      <form>
+        <div className="flex flex-col md:flex-row md:gap-3">
+          <input type="text" name="name" placeholder="Full Name" />
+          <input type="email" placeholder="Email Address"  name="email"  />
+        </div>
+        <textarea name="message" placeholder="Write Your Message" />
+        <button type="submit" className="bg-white w-full text-black py-3 rounded-md ">Send Message</button>
+      </form>
     </section>
   )
 }
