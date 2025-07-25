b616943c6fa1cbd11f6d493500b2d83449a3559a
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-07-25 16:09:24 +0100

    Update: resume file

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,7 @@ const Contact: React.FC = () => {
       <h1>Get in Touch.</h1>
       <div className="flex flex-wrap justify-center gap-5 my-5">
         <span className="det-span"><MailIcon fill="white" className="mr-2 w-[15px] h-[15px]"/>idyegenathaniel@gmail.com</span>
-        <span className="det-span"><PhoneIcon fill="white" className="mr-2 w-[15px] h-[15px]"/>+234 9032461074</span>
+        <span className="det-span"><PhoneIcon fill="white" className="mr-2 w-[15px] h-[15px]"/>+234 903 246 1074</span>
       </div>
       <form method="POST" action="https://getform.io/f/arolrwkb">
         <div className="flex flex-col md:flex-row md:gap-3">
