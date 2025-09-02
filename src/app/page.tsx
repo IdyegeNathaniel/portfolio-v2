92b35c975fee587930f4bb41dcd7543bc3b327c3
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-09-02 14:04:01 +0100

    Update: Colour Scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="dark:bg-gradient-to-tr from-neutral-950 to-neutral-700 dark:text-white p-10 ">
+    <div className="bg-gradient-to-tr from-neutral-950 to-neutral-700 text-white p-10 ">
       <Navbar />
       <Hero />
       <About />
